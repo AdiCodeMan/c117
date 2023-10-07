9e0fa1b3b94622d0b0879a5d610e2acff75ae309
AdiCodeMan <123280392+AdiCodeMan@users.noreply.github.com>
2023-10-06 19:42:59 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+random_no = Math.floor((Math.random()*array_1.length)+1);
+
+console.log(random_number);
+sketch=document.getElementById("STBD");
+timer_counter=0;
+timer_check="";
+drawn_sketch="";
+answer_holder="";
+score=0;
